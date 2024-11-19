00d5926ffe569d2b615652363ec9668e1c76f9ae
kozalakisk <kozalakisk@gmail.com>
2024-11-19 00:57:50 -0800

    Added Supra ISI MMR

--- Scripts/Functions.py
+++ Scripts/Functions.py
@@ -1,4 +1,5 @@
 from scipy.optimize import curve_fit
+from scipy.stats import norm
 from collections import Counter
 from matplotlib import colors
 import matplotlib.pyplot as plt
@@ -25,10 +26,16 @@ type_colors_B = {'ITL46': 'limegreen',
                  'SST': 'deepskyblue',
                  'VIP': 'cadetblue'}
 
-def Read_Tables(files, frequency, amplitude, phase = False, maximal = False):
+def Read_Tables(files, frequency, amplitude, phase = False, maximal = False, sweeps = None):
     dfs = []
-    for i in files:
-        dfs.append(pd.read_csv('../Results/Result_Tables/' + i + '.csv'))
+    for s,i in enumerate(files):
+        if sweeps is not None:
+            if 'MMR' in sweeps[s] or 'VL' in sweeps[s]:
+                dfs.append(pd.read_pickle('../Results/Result_Tables/' + i + '.pkl'))
+            else:
+                dfs.append(pd.read_csv('../Results/Result_Tables/' + i + '.csv'))
+        else:
+            dfs.append(pd.read_csv('../Results/Result_Tables/' + i + '.csv'))
     columns = dfs[0].columns
     for i in range(len(dfs)):
         dfs[i] = dfs[i].groupby('ES_frequency(hz)').get_group(frequency)
@@ -41,30 +48,39 @@ def Read_Tables(files, frequency, amplitude, phase = False, maximal = False):
         cell_sets = []
         max_set = 0
         for i in dfs:
-            cell_sets.append(set(i.loc[:,'Cell_ID']))
+            cell_sets.append(set(i.loc[:,'Cell_ID'].astype(int)))
             if max_set < len(cell_sets[-1]):
                 max_set = len(cell_sets[-1])
         common_cells = cell_sets[0]
         for s in cell_sets[1:]:
             common_cells = common_cells.intersection(s)
         for i in range(len(dfs)):
-            dfs[i] = dfs[i][dfs[i]['Cell_ID'].isin(common_cells)]
+            dfs[i] = dfs[i][dfs[i]['Cell_ID'].astype(int).isin(common_cells)]
         if max_set != len(common_cells):
             print ("WARNING: Reduced cells from "+str(max_set)+" to "+str(len(common_cells))+" due to mismatch.")     
+    
     df = dfs[0].iloc[:,:3].copy().reset_index(drop = True)
     for i in range(len(dfs)):
-        assert list(df.loc[:,'Cell_ID'])==list(dfs[i].loc[:,'Cell_ID'])
+        assert list(df.loc[:,'Cell_ID'])==list(dfs[i].loc[:,'Cell_ID'].astype(int))
         if phase:
             df.loc[:,files[i]] = (dfs[i].loc[:,'Phs@40000(deg)'].reset_index(drop = True)+270)%360
             if maximal:
                 df.loc[:,'Amplitude_'+files[i]] = dfs[i].loc[:,'Amp_Median(mV)'].reset_index(drop = True)
         else: 
-            df.loc[:,files[i]] = dfs[i].loc[:,'Amp_Median(mV)'].reset_index(drop = True)
+            if sweeps is not None:
+                if 'VL' in sweeps[i]:
+                    df.loc[:,files[i]] = dfs[i].loc[:,'mean_vector_length'].reset_index(drop = True)
+                elif 'MMR' in sweeps[i]:
+                    df.loc[:,files[i]] = dfs[i].loc[:,'MMR'].reset_index(drop = True)
+                else:
+                    df.loc[:,files[i]] = dfs[i].loc[:,'Amp_Median(mV)'].reset_index(drop = True)
+            else:
+                df.loc[:,files[i]] = dfs[i].loc[:,'Amp_Median(mV)'].reset_index(drop = True)
     return (df)
 
 def Get_cell_type(dfcells, cell_type):
     dfcell = dfcells.get_group(cell_type)
-    dfcell.loc[:,'Cell_Rotation(deg)'] = dfcell['Cell_Rotation(deg)']*np.pi/180
+    dfcell.loc[:,'Cell_Rotation(deg)'] = dfcell['Cell_Rotation(deg)'].astype('int')*np.pi/180
     newdf = dfcell.copy()
     newdf.sort_values(by=['Cell_Rotation(deg)'], inplace = True)
     return newdf.copy()
@@ -185,11 +201,13 @@ def getfitset(xdata, ydata, funct):
 
 def Compare(files, sweeps, sweep_colors, morphologies_paths = None, 
                                                 Theoretical = None, Fit = False, frequency = 1,amplitude = 10):
-    df = Read_Tables(files,frequency,amplitude)                                  # frequency Hz, amplitude uA
+    df = Read_Tables(files,frequency,amplitude, sweeps = sweeps)                                  # frequency Hz, amplitude uA
     allresults = []
     if Fit:
         allresults.append(['Type', 'ID', 'angledrift', 'topness', 'sideness', 'amplitude'])
     for sp in sweep_colors[sweeps[0]]:
+        if sp not in df.groupby('Cell_Type').groups.keys():
+            continue
         dft = df.groupby('Cell_Type').get_group(sp)
         dfcells = dft.groupby('Cell_ID')
         fig, axs = Make_figure(dfcells, sp)
@@ -526,3 +544,307 @@ def Compare_theoretical(cell_list, frequency = 1, amplitude = 10, axconf = 'NA_'
     ax.legend(legend_handles, leg_colors.keys(), loc='center')
     ax.axis('off')
     plt.show()
+    
+def Plot_isi_distributions(target, results):
+    tar = target.groupby('cell_type')
+    res = results.groupby('ES_frequency(hz)').get_group(8).groupby('ES_current(uA)').get_group(0)
+    res['Cell_Rotation(deg)'] = res['Cell_Rotation(deg)'].astype(int)
+    res['Cell_ID'] = res['Cell_ID'].astype(int)
+    res = res.groupby('Cell_Rotation(deg)').get_group(0).groupby('Cell_Type')
+
+    fig, axs = plt.subplots(2, 3, figsize=(15, 10))
+    axs = axs.flatten()
+
+    index_flag = 0
+    for idx, ctype in enumerate(type_colors_A):
+        if 'LAMP' in ctype:
+            index_flag += 1
+            continue
+        means = list(tar.get_group(ctype)['Target'])
+        stds = np.array(tar.get_group(ctype)['TargetStD'])/2
+        isis = res.get_group(ctype)['ISIs']
+        isi = []
+        for i in isis:
+            for j in i:
+                isi.append(j)
+        
+        if 'ITL' in ctype:
+            x = np.linspace(0, 55, 1000)
+            rng = [0, 55]
+        elif 'PVALB':
+            x = np.linspace(0, 300, 1000)
+            rng = [0, 250]
+        else:
+            x = np.linspace(0, 200, 1000)
+            rng = [0, 200]
+        
+        overall_distribution = np.zeros_like(x)
+        
+        for mu, sigma in zip(means, stds):
+            overall_distribution += norm.pdf(x, mu, sigma)
+        overall_distribution  /= np.trapz(overall_distribution, x)
+        axs[idx - index_flag].hist(isi, bins=40, range=rng, density=True, alpha=0.5, label = "Simulation spikes",
+                      color=type_colors_B[ctype], edgecolor='black') 
+        axs[idx - index_flag].plot(x, overall_distribution, label='Target distribution', color='k', linewidth=2)
+        
+        axs[idx - index_flag].grid(True)
+        axs[idx - index_flag].set_xlim(rng)
+        if 'IT' in ctype:
+            axs[idx - index_flag].set_ylim(0,0.2)
+        else:
+            axs[idx - index_flag].set_ylim(0,0.034)
+        axs[idx - index_flag].set_xlabel('Firing rate (Hz)')
+        axs[idx - index_flag].set_ylabel('Number of spikes normalized')
+        axs[idx - index_flag].set_title(ctype)
+        axs[idx - index_flag].legend()
+
+    plt.tight_layout()
+    plt.show()
+    
+def Phase_Artifact (FiringRate,ESRate, bins = 15):
+    PA = ((1/FiringRate)%(1/ESRate))*360/(1/ESRate)
+    modes = 1
+    phase = PA
+    while phase%360>(360/bins):
+        modes += 1
+        phase += PA
+    drift = phase%(360/bins)
+    return ((1/FiringRate)%(1/ESRate))*360/(1/ESRate), modes, drift
+    
+    
+def Plot_entrainment (data_frame, targets = None, MMR = False, subtract_ref = False, bins = 15):
+    all_results = []
+    if targets is not None:
+        tar = targets.set_index('cell_id')[['PM', 'PS']]
+    res = data_frame.copy()
+    res['Cell_Rotation(deg)'] = res['Cell_Rotation(deg)'].astype(int)
+    res['Cell_ID'] = res['Cell_ID'].astype(int)
+    rotation  =  0 # deg
+    gftex = res.groupby('Cell_Type')
+    for typeex, gftexf in gftex:
+        gfa = gftexf.groupby('Cell_Rotation(deg)').get_group(rotation)
+        gfa = gfa.groupby('Cell_ID')
+        for cell, gfc in gfa:
+            fig = plt.figure(figsize = (8,6))
+            if targets is not None:
+                fig.suptitle(gfc['Cell_Type'].iloc[0] + ' ' + str(gfc['Cell_ID'].iloc[0]) +
+                ' \nFiring rate: ' + str(round(tar.loc[cell,'PM'],2)) + ' +- ' + str(round(tar.loc[cell,'PS'],2)) + ' Hz', fontsize = 16)
+            else:
+                fig.suptitle(gfc['Cell_Type'].iloc[0] + ' ' + str(gfc['Cell_ID'].iloc[0]), fontsize = 16)
+            color = type_colors_B[str(gfc['Cell_Type'].iloc[0])]
+            for i,freq in enumerate([8,28,140]):
+                for j,ampl in enumerate([0,10,100,500]):
+                    ax = fig.add_subplot(3,4,1+j+i*4, projection='polar')
+                    gft = gfc.groupby('ES_frequency(hz)').get_group(freq).groupby('ES_current(uA)').get_group(ampl)
+                    if bins==15:
+                        X, Y = gft['VL_X'].iloc[0], gft['VL_Y'].iloc[0]
+                        if subtract_ref:
+                            if ampl==0:
+                                reference = Y
+                            else:
+                                Y -= reference
+                    else:
+                        Phases = gft['Phases'].iloc[0]
+                        radians = np.asarray(Phases)*np.pi/180
+                        mean_phase_rad = pcs.descriptive.mean(np.array(radians))
+                        mean_phase_angle = mean_phase_rad*(180 / np.pi)
+                        mean_pvalue_z=pcs.rayleigh(np.array(radians))
+                        mean_vector_length = pcs.descriptive.vector_strength(np.array(radians))
+                        Y, X = np.histogram(Phases, bins=np.linspace(0,360,bins+1))
+                        Xp =(X[1:] + X[:-1]) / 2
+                        X = Xp * np.pi / 180
+                        Y = np.true_divide(Y, (np.max(Y)))
+                        if subtract_ref:
+                            if ampl==0:
+                                reference = Y
+                            else:
+                                Y -= reference
+                    bars = ax.bar(X, Y,  width=0.4, edgecolor = 'black', color=color, alpha=0.8, linewidth=1.2)
+                    ax.set_axisbelow(True)
+                    thetaticks = np.arange(0,360,90)
+                    radius = [0,gft['mean_vector_length'].iloc[0]]
+                    if MMR:
+                        radius = [0,gft['MMR'].iloc[0]]
+                        th = gft['MMR_X'].iloc[0][list(gft['MMR_Y'].iloc[0]).index(max(list(gft['MMR_Y'].iloc[0])))]
+                        theta = [th,th]
+                        ax.plot(theta,radius,type_colors_A[str(gfc['Cell_Type'].iloc[0])],linewidth = 2)
+                        
+                        ax.set_yticks([])
+                        ax.set_xticks(np.pi/180. * np.linspace(45,  405, 2, endpoint=False))
+                        ax.set_xlabel('MMR: {:.3f}'.format(gft['MMR'].iloc[0]) +  
+                                          '\nAngle:  {:.3f}'.format(th*180/np.pi))
+                    else:
+                        theta = [gft['mean_phase_angle'].iloc[0]*np.pi/180,gft['mean_phase_angle'].iloc[0]*np.pi/180]
+                        ax.plot(theta,radius,type_colors_A[str(gfc['Cell_Type'].iloc[0])],linewidth = 2)
+                        ax.set_yticks([])
+                        ax.set_xticks(np.pi/180. * np.linspace(45,  405, 2, endpoint=False))
+                        ax.set_xlabel('VL: {:.3f}'.format(gft['mean_vector_length'].iloc[0]) + 
+                                          '\nPval: {:.3f}'.format(gft['mean_pvalue_z'].iloc[0][0]) + 
+                                          '\nAngle:  {:.3f}'.format(gft['mean_phase_angle'].iloc[0]))
+                    if i==0:
+                        ax.set_title(str(ampl) + ' uA', fontsize = 14)
+                    if j==0:
+                        ax.set_ylabel(str(freq) + ' Hz', fontsize = 14)
+                    elif targets is not None and j==1:
+                        PA, modes, drift = Phase_Artifact(tar.loc[cell,'PM'],freq, bins = bins)
+                        ax.set_ylabel('PA: '+ str(round(PA,1)) + 'deg\nSteps: '+ str(modes) +'\n', fontsize = 14)
+                        all_results.append([PA, modes, drift, gft['MMR'].iloc[0]])
+            fig.tight_layout()  
+            plt.show()
+    return pd.DataFrame(all_results)
+            
+def Plot_entrainment_across (data_frame, show_all = True, mean = 'None', metric_tag = 'mean_vector_length'):
+    
+        
+    dff = data_frame.groupby('Cell_Rotation(deg)').get_group('0')
+    if metric_tag == 'ISIs':
+        if mean == 'median':
+            dff['ISIs'] = [np.median(i) for i in dff['ISIs']]
+        else:
+            dff['ISIs'] = [np.mean(i) for i in dff['ISIs']]
+    for freq in [8,28,140]:
+        df = dff.groupby('ES_frequency(hz)').get_group(freq)
+        df = df.groupby('Cell_Type')
+        fig, ax = plt.subplots(figsize=(8, 4))
+        for name, group in df:
+            if mean == 'mean':
+                x = ['0','10','100','500']
+                y = list(group.groupby(['ES_current(uA)'])[metric_tag].mean())
+                if not show_all:
+                    x = [x[0],x[-1]]
+                    y = [y[0],y[-1]]
+                y = y-np.mean(y)
+                ax.plot(x, y, label=name, color = type_colors_B[name])
+                ax.set_ylabel('Mean '+ metric_tag, fontsize = 16)
+            elif mean == 'median':
+                x = ['0','10','100','500']
+                y = list(group.groupby(['ES_current(uA)'])[metric_tag].median())
+                if not show_all:
+                    x = [x[0],x[-1]]
+                    y = [y[0],y[-1]]
+                y = y-np.mean(y)
+                ax.plot(x, y, label=name, color = type_colors_B[name])
+                ax.set_ylabel('Median '+ metric_tag, fontsize = 16)
+            else:
+                dfc = group.groupby('Cell_ID')
+                leg_flag = 1
+                for n, gr in dfc:
+                    gr['ES_current(uA)'] = gr['ES_current(uA)'].astype(str)  # Correctly convert to string
+                    if leg_flag:
+                        ax.plot(gr['ES_current(uA)'], gr[metric_tag]-np.mean(gr[metric_tag]), label=name, color = type_colors_B[name])
+                        leg_flag = 0
+                    else:
+                        ax.plot(gr['ES_current(uA)'], gr[metric_tag]-np.mean(gr[metric_tag]), label=None)
+                ax.set_ylabel('All '+ metric_tag, fontsize = 16)
+        if metric_tag == 'ISIs':
+            ax.set_ylim(-5,5)
+        ax.set_xlabel('ES_current(uA)', fontsize = 16)
+        ax.set_title('ES_frequency: ' + str(freq) + ' Hz', fontsize = 16)
+        ax.legend()
+        plt.show()
+
+def MMR (PhaseList):
+    instances = np.array(np.round(PhaseList), dtype = int)
+    bin_size = 36  
+    gaussian = []
+    for i in range(bin_size*2+1):
+        gaussian.append(np.exp(-0.5*(i-bin_size)**2/(bin_size/4)**2)/(bin_size*np.sqrt(2*np.pi)/4))
+    gaussian = np.array(gaussian)/gaussian[bin_size]
+    waveshaped = []
+    for i in instances:
+        waveshape = np.zeros(360)
+        indexes = np.arange(0,len(gaussian))
+        indexes = indexes+i-bin_size
+        for j in np.arange(0,len(gaussian)):
+            if indexes[j]<0:
+                indexes[j] +=360
+            elif indexes[j]>359:
+                indexes[j] -=360
+        for j, ind in enumerate(indexes):
+            waveshape[ind] = gaussian [j]
+        waveshaped.append(waveshape)
+    sumation = np.sum(waveshaped, axis = 0)
+    sumation = sumation/np.max(sumation)
+    return sumation, 1-np.mean(sumation)
+        
+def Plot_Displacement(time_displace = False, entrainment = False, num_bins = 15):
+    if entrainment:
+        num_bins = 20
+
+        fig, ax = plt.subplots(2,6,figsize = (12,6), subplot_kw={'projection': 'polar'})
+        for b, bin_move in enumerate([2,4]):
+            vls = []
+            mmrs = []
+            for s, spike_move in enumerate([0,10,20,30]):
+                values = np.full(num_bins, 100)
+                angles = np.linspace(0, 2 * np.pi, num_bins, endpoint=False)
+                bins_away = -(np.sin(angles)*bin_move).astype(int)
+                spikes_moved = np.abs(np.sin(angles))*spike_move
+                for i in range(len(values)):
+                    values[i] -= spikes_moved[i]
+                    if bins_away[i]>0 and i<len(values)-bins_away[i]:
+                        values[i+bins_away[i]] += spikes_moved[i]
+                    elif bins_away[i]<0 and i>=bins_away[i]:
+                        values[i+bins_away[i]] += spikes_moved[i]
+                    elif bins_away[i]>0:
+                        values[(i+bins_away[i])%len(values)] += spikes_moved[i]
+                    elif bins_away[i]<0:
+                        values[len(values)+i+bins_away[i]] += spikes_moved[i]
+                Phases = []
+                for i in range(len(angles)):
+                    for j in range(values[i]):
+                        Phases.append(angles[i]*180/np.pi)
+                radians = np.asarray(Phases)*np.pi/180
+                mean_phase_rad = pcs.descriptive.mean(np.array(radians))
+                mean_phase_angle = mean_phase_rad*(180 / np.pi)
+                mean_pvalue_z=pcs.rayleigh(np.array(radians))
+                mean_vector_length = pcs.descriptive.vector_strength(np.array(radians))
+                cd, mmr = MMR(Phases)
+                ax[b][s].bar(angles, values, width=2 * np.pi / num_bins, bottom=0.0, color='b', edgecolor='black')
+                ax[b][s].grid(False)
+                ax[b][s].set_yticklabels([])
+                ax[b][s].set_xlabel('VL: ' + str(round(mean_vector_length,3))+'\nMMR: ' + str(round(mmr,3)))
+                ax[b][s].set_title('Case: '+str(s%4)+'\nMove '+str(spike_move)+', '+str(bin_move)+' bins')
+                vls.append(round(mean_vector_length,3))
+                mmrs.append(round(mmr,3))
+            ax[b][4].axis('off')
+            ax[b][5].axis('off')
+            ax1 = fig.add_subplot(2,3,3*(b+1))
+            ax1.plot(vls, label = 'VL')
+            ax1.plot(mmrs, label = 'MMR')
+            ax1.set_xlabel('Case')
+            ax1.set_ylabel('Length')
+            ax1.legend()
+        plt.tight_layout()
+        plt.show()
+    else:
+        values = np.full(num_bins, 100)
+        angles = np.linspace(0, 2 * np.pi, num_bins, endpoint=False)
+
+        fig, ax = plt.subplots(1,2,subplot_kw={'projection': 'polar'})
+        ax[0].bar(angles, values, width=2 * np.pi / num_bins, bottom=0.0, color='b', edgecolor='black')
+        ax[0].grid(False)
+        ax[0].set_yticklabels([])
+        ax[0].set_title('Control')
+
+        if time_displace:
+            bins_away = -(np.sin(angles)*3).astype(int)
+            spikes_moved = np.abs(np.sin(angles))*30
+            for i in range(len(values)):
+                values[i] -= spikes_moved[i]
+                if bins_away[i]>0 and i<len(values)-bins_away[i]:
+                    values[i+bins_away[i]] += spikes_moved[i]
+                elif bins_away[i]<0 and i>=bins_away[i]:
+                    values[i+bins_away[i]] += spikes_moved[i]
+                elif bins_away[i]>0:
+                    values[(i+bins_away[i])%len(values)] += spikes_moved[i]
+                elif bins_away[i]<0:
+                    values[len(values)+i+bins_away[i]] += spikes_moved[i]
+        else:
+            values = np.full(num_bins, 100)+np.cos(angles)*40
+        ax[1].bar(angles, values, width=2 * np.pi / num_bins, bottom=0.0, color='b', edgecolor='black')
+        ax[1].grid(False)
+        ax[1].set_yticklabels([])
+        ax[1].set_title('With ES')
+        plt.tight_layout()
+        plt.show()
